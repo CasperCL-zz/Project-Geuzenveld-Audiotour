5e1387e01dce313196f14954196909b2b755876e
Casper <casperjeekhof@gmail.com>
2013-03-11 21:08:02 +0100

    Changed background at Splash

--- Audiotour Geuzenveld/src/com/hetfotogeniekegeluid/activity/SplashActivity.java
+++ Audiotour Geuzenveld/src/com/hetfotogeniekegeluid/activity/SplashActivity.java
@@ -3,6 +3,7 @@ package com.hetfotogeniekegeluid.activity;
 import com.hetfotogeniekegeluid.R;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 /** This is where is all starts.
@@ -16,5 +17,21 @@ public class SplashActivity extends Activity {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash);
+		
+		Thread timer = new Thread(){
+			
+			@Override
+			public void run(){
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				Intent mainActivity = new Intent(SplashActivity.this, MainActivity.class);
+				startActivity(mainActivity);
+			}
+		};
+		timer.start();
 	}
 }
